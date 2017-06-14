f33efa0865991dd64c99a18141eada1e620a9555
victorklymec <victorklymec@gmail.com>
2017-06-14 20:10:12 +0300

    Simple commit

---
+++ Empty/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Empty.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Empty.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning! " : "Good Afternoon";
+            return View();
+        }
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse questResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Thanks", questResponse);
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
